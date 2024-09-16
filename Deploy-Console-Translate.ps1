fbe1f6da8a4795d7bcfff3079a429b50bcf9db86
Lifailon <deadepicboss@gmail.com>
2024-09-17 00:21:55 +0300

    update deploy

--- Deploy-Console-Translate.ps1
+++ Deploy-Console-Translate.ps1
@@ -1,4 +1,3 @@
-### Install/Update Module
 if ($IsLinux) {
     $path = ($env:PSModulePath.Split(":")[0])+"/Console-Translate"
 } else {
@@ -18,22 +17,4 @@ foreach ($File_GitHub in $Files_GitHub) {
     $Url_Download = $File_GitHub.download_url
     #(New-Object Net.WebClient).DownloadString($Url_Download) | Out-File "$Module_Path\$File_Name" -Encoding default -Force
     Invoke-RestMethod -Uri $Url_Download -OutFile "$Module_Path\$File_Name"
-}
-### Install/Update DeepLX
-if ($IsLinux) {
-    $DeepLX_Path = "$path/$Module_Version/deeplx"
-    $DeepLX_Releases_Latest = Invoke-RestMethod "https://api.github.com/repos/OwO-Network/DeepLX/releases/latest"
-    [string]$DeepLX_Download_url = $($DeepLX_Releases_Latest.assets | Where-Object Name -Match "linux_amd64").browser_download_url
-} else {
-    $DeepLX_Path = "$path\$Module_Version\deeplx.exe"
-    $DeepLX_Releases_Latest = Invoke-RestMethod "https://api.github.com/repos/OwO-Network/DeepLX/releases/latest"
-    [string]$DeepLX_Download_url = ($DeepLX_Releases_Latest.assets | Where-Object Name -Match "amd64.exe").browser_download_url
-}
-#(New-Object Net.WebClient).DownloadString($DeepLX_Download_url) | Out-File $DeepLX_Path -Encoding default -Force
-Invoke-RestMethod -Uri $DeepLX_Download_url -OutFile $DeepLX_Path
-if ($IsLinux) {
-    chmod +x $DeepLX_Path
-} else {
-    "`nNew-Alias -Name gt -Value Get-Translate -Force" >> $PROFILE
-    "`nNew-Alias -Name gd -Value Get-DeepLX -Force" >> $PROFILE
 }
